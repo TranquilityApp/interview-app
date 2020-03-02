813974ebdafe64a4ebc285fc06a53718ff071755
Craig Chisholm <chisholm.craig@gmail.com>
2020-03-02 10:12:16 -0400

    prepare for interview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Todos from './Todos';
@@ -8,73 +7,63 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const itemsLoading = (state = false, action) => {
-  switch (action.type) {
-    case 'ITEMS_LOADING':
-      return true
-    case 'ITEMS_LOADED':
-    case 'ADD_NEW_ITEM':
-    case 'REMOVE_ITEM':
-    case 'EDIT_ITEM':
-      return false
-    default:
-      return state;
-  }
-}
-
 const activeEditItemId = (state = null, action) => {
   switch (action.type) {
     case 'START_EDIT_ITEM':
-      return action.id
+      return action.id;
     case 'FINISH_EDIT_ITEM':
-      return null
+      return null;
     default:
-      return state
+      return state;
   }
-}
+};
 
-const itemsReducer = (state = [{ id: 1, description: 'My Task', completed: true }], action) => {
+const itemsReducer = (
+  state = [{ id: 1, description: 'My Task', completed: true }],
+  action
+) => {
   switch (action.type) {
     case 'ADD_NEW_ITEM':
-      return [...state, { ...action.item, id: state.length + 1 }]
+      return [...state, { ...action.item, id: state.length + 1 }];
     case 'REMOVE_ITEM':
       return state.filter(item => {
-        return item.id !== action.id
-      })
+        return item.id !== action.id;
+      });
     case 'EDIT_ITEM':
       return state.map(item => {
         if (item.id === action.item.id) {
-          return {...item, description: action.item.description}
+          return { ...item, description: action.item.description };
         }
-        return item
-      })
+        return item;
+      });
     case 'TOGGLE_ITEM_COMPLETION':
       return state.map(item => {
         if (item.id === action.id) {
-          return {...item, completed: !item.completed }
+          return { ...item, completed: !item.completed };
         }
-        return item
-      })
+        return item;
+      });
     default:
-      return state
+      return state;
   }
-}
+};
 
 const reducers = combineReducers({
   todos: itemsReducer,
-  todosLoading: itemsLoading,
   activeEditItemId: activeEditItemId
-})
+});
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
-
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
           <h2>ToDo List</h2>
         </div>
         <Provider store={store}>
--- src/Todos.js
+++ src/Todos.js
@@ -4,41 +4,31 @@ import { connect } from 'react-redux';
 import ItemForm from './ItemForm';
 import ItemList from './ItemList';
 
-const addItem = (item) => {
-  return (dispatch) => {
-    dispatch({type: 'ITEMS_LOADING'});
-    setTimeout(() => {
-      dispatch({type: 'ADD_NEW_ITEM', item});
-    }, 1500)
-  }
-}
+const addItem = item => {
+  return dispatch => {
+    dispatch({ type: 'ADD_NEW_ITEM', item });
+  };
+};
 
-const editItem = (item) => {
-  return (dispatch) => {
-    dispatch({type: 'ITEMS_LOADING'});
-    setTimeout(() => {
-      dispatch({type: 'EDIT_ITEM', item});
-    }, 1500)
-  }
-}
+const editItem = item => {
+  return dispatch => {
+    dispatch({ type: 'EDIT_ITEM', item });
+  };
+};
 
-const removeItem = (id) => {
-  return (dispatch) => {
-    dispatch({type: 'ITEMS_LOADING'});
-    setTimeout(() => {
-      dispatch({
-        type: 'REMOVE_ITEM',
-        id: id
-      })
-    }, 1500)
-  }
-}
+const removeItem = id => {
+  return dispatch => {
+    dispatch({
+      type: 'REMOVE_ITEM',
+      id: id
+    });
+  };
+};
 
 class Todos extends Component {
   static propTypes = {
-    items: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-  }
+    items: PropTypes.array.isRequired
+  };
 
   constructor() {
     super();
@@ -46,61 +36,67 @@ class Todos extends Component {
     // this.state = { editItem: null }
   }
 
-  onNewItemSubmited = (item) => {
-    this.props.dispatch(addItem(item))
-  }
+  onNewItemSubmited = item => {
+    this.props.dispatch(addItem(item));
+  };
 
-  onEditItemSubmited = (item) => {
-    this.props.dispatch(editItem(item))
+  onEditItemSubmited = item => {
+    this.props.dispatch(editItem(item));
     this.props.dispatch({
       type: 'FINISH_EDIT_ITEM'
-    })
-  }
+    });
+  };
 
-  toggleItemCompletion = (id) => {
+  toggleItemCompletion = id => {
     this.props.dispatch({
-      type: "TOGGLE_ITEM_COMPLETION",
-      id:   id
-    })
-  }
+      type: 'TOGGLE_ITEM_COMPLETION',
+      id: id
+    });
+  };
 
-  startEditItem = (id) => {
+  startEditItem = id => {
     this.props.dispatch({
       type: 'START_EDIT_ITEM',
       id: id
-    })
-  }
+    });
+  };
 
-  removeItem = (id) => {
-    this.props.dispatch(removeItem(id))
-  }
+  removeItem = id => {
+    this.props.dispatch(removeItem(id));
+  };
 
   render() {
     const editItem = this.props.items.filter(item => {
-      return item.id === this.props.activeEditItemId
-    })[0]
+      return item.id === this.props.activeEditItemId;
+    })[0];
 
     return (
       <div>
         <div>
-          <ItemForm onNewItemSubmited={this.onNewItemSubmited} onEditItemSubmited={this.onEditItemSubmited} editItem={editItem} />
+          <ItemForm
+            onNewItemSubmited={this.onNewItemSubmited}
+            onEditItemSubmited={this.onEditItemSubmited}
+            editItem={editItem}
+          />
         </div>
         <div>
-          {
-            this.props.loading ? <p>Loading...</p> : <ItemList items={this.props.items} onTogglComplete={this.toggleItemCompletion} onEditItem={this.startEditItem} onRemoveItem={this.removeItem}/>
-          }
+          <ItemList
+            items={this.props.items}
+            onTogglComplete={this.toggleItemCompletion}
+            onEditItem={this.startEditItem}
+            onRemoveItem={this.removeItem}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     items: state.todos,
-    loading: state.todosLoading,
     activeEditItemId: state.activeEditItemId
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(Todos)
+export default connect(mapStateToProps)(Todos);
